1820be302f749c4a05000b822f1f3b905e31e217
Unknown <joshua.tobias@games-academy.de>
2019-02-19 14:49:19 +0100

    Interface

---
+++ CircleBuffer/CircleBuffer/ICircularBuffer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularBuffer
+{
+    interface ICircularBuffer
+    {
+        int Capacity { get; set; }
+        int Count { get; set; }
+        bool IsEmpty { get; set; }
+        bool IsFull { get; set; }
+
+        void Produce(IEnumerable T);
+        IEnumerable Consume();
+        void Clear();
+        int ProduceAll(IEnumerable T);
+        void ClearAll();
+        void WaitProduce(IEnumerable T);
+        IEnumerable WaitConsume();
+    }
+}
